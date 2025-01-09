b00ff03b6b8e5015811765262c7a949bf7fce215
Mathieu <bon.mathieu@gmail.com>
2025-01-09 14:20:57 +0100

    Rider checklist now updates selected values when filters selected.

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from data_parser import parse_event_data
 
 
 json_file_path = os.path.join(
-    os.path.dirname(__file__), "..", "data", "miami_2019.json"
+    os.path.dirname(__file__), "..", "data", "miami_2024.json"
 )
 json_file = open(json_file_path)
 data = json.load(json_file)
@@ -26,55 +26,51 @@ app = Dash(name=__name__, external_scripts=["https://cdn.tailwindcss.com"])
 app.layout = PageLayout(data=data, df=df).layout
 
 
+
 @app.callback(
+    Output(component_id="graph", component_property="figure"),
     Output("names_checklist", "value"),
     Output("country_dropdown", "value"),
     Output("division_dropdown", "value"),
     Output("discipline_dropdown", "value"),
     Output("age_dropdown", "value"),
+    Input("names_checklist", "value"),
+    Input("country_dropdown", "value"),
+    Input("division_dropdown", "value"),
+    Input("discipline_dropdown", "value"),
+    Input("age_dropdown", "value"),
     Input("top_10_riders_button", "n_clicks"),
     Input("all_riders_button", "n_clicks"),
     State("names_checklist", "options"),
     prevent_initial_call=True,
 )
-def update_filters(top_10_clicks, reset_clicks, names_options):
-    ctx = dash.callback_context
-
-    if not ctx.triggered:
-        return dash.no_update
-
-    button_id = ctx.triggered[0]["prop_id"].split(".")[0]
-
-    if button_id == "top_10_riders_button":
-        top_10_riders = (
-            df[(df["rank"] >= 1) & (df["rank"] <= 10)]["name"].unique().tolist()
-        )
-
-        return (
-            top_10_riders,
-            dash.no_update,
-            dash.no_update,
-            dash.no_update,
-            dash.no_update,
-        )
-    elif button_id == "all_riders_button":
-        return df["name"].unique().tolist(), None, None, None, None
-
-    return dash.no_update
-
-
-@app.callback(
-    Output(component_id="graph", component_property="figure"),
-    Input(component_id="names_checklist", component_property="value"),
-    Input(component_id="country_dropdown", component_property="value"),
-    Input(component_id="division_dropdown", component_property="value"),
-    Input(component_id="discipline_dropdown", component_property="value"),
-    Input(component_id="age_dropdown", component_property="value"),
-)
-def update_line_chart(
-    riders, selected_countries, selected_divisions, selected_disciplines, selected_ages
+def update_graph_and_checklist(
+    riders,
+    selected_countries,
+    selected_divisions,
+    selected_disciplines,
+    selected_ages,
+    top_10_clicks,
+    reset_clicks,
+    names_options,
 ):
-    filtered_df = df[df["name"].isin(riders)]
+    ctx = dash.callback_context
+    filtered_df = df.copy()
+
+    # Determine which button triggered the callback
+    if ctx.triggered:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        if button_id == "top_10_riders_button":
+            filtered_df = df[(df["rank"] >= 1) & (df["rank"] <= 10)]
+            top_10_riders = filtered_df["name"].unique().tolist()
+            figure = LineChart(df=filtered_df).figure
+            return figure, top_10_riders, None, None, None, None
+
+        elif button_id == "all_riders_button":
+            filtered_df = df.copy()
+            all_riders = filtered_df["name"].unique().tolist()
+            figure = LineChart(df=filtered_df).figure
+            return figure, all_riders, None, None, None, None
 
     filters = [
         {"name": "country", "content": selected_countries},
@@ -84,16 +80,25 @@ def update_line_chart(
     ]
 
     for filter in filters:
-        print(f"filter name : {filter['name']}, content: {filter['content']}")
         if filter["content"]:
-            print(f"Content in filter {filter['name']} : {filter['content']}")
             if not isinstance(filter["content"], list):
                 content = [filter["content"]]
+            else:
+                content = filter["content"]
             filtered_df = filtered_df[filtered_df[filter["name"]].isin(content)]
-            print(f"df filtered with {filter['name']} filtered_df")
 
     figure = LineChart(df=filtered_df).figure
-    return figure
+
+    updated_checklist_values = filtered_df["name"].unique().tolist()
+
+    return (
+        figure,
+        updated_checklist_values,
+        dash.no_update,
+        dash.no_update,
+        dash.no_update,
+        dash.no_update,
+    )
 
 
 if __name__ == "__main__":
