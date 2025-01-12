ddc07e3071d76e82535954914a3322d94421fe99
Mathieu <bon.mathieu@gmail.com>
2025-01-12 13:19:16 +0100

    Functional event selection. Code badly optimized though.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from dash import Dash, Input, Output, State, ctx, dcc
+from dash import Dash, Input, Output, State, ctx, html
+import flag
 import dash
 from components.PageLayout import PageLayout
 from components.PxLineChart import PxLineChart
@@ -7,17 +8,32 @@ from utils.path_utils import events_files
 from utils.json_utils import get_event_data_from_json
 
 
+app = Dash(name=__name__, external_scripts=["https://cdn.tailwindcss.com"])
+
+
 event_data = get_event_data_from_json(events_files[-1])
 df = parse_event_data(event_data)
 
 
-app = Dash(name=__name__, external_scripts=["https://cdn.tailwindcss.com"])
-
 app.layout = PageLayout(event_data=event_data, df=df, data_filenames=events_files)
+
+
+def get_riders_list_from_df(df) -> list[str]:
+    return df["name"].unique().tolist()
 
 
 @app.callback(
     Output("graph", "figure", allow_duplicate=True),
+    Output("names_checklist", "options"),
+    Output("names_checklist", "value", allow_duplicate=True),
+    Output("country_dropdown", "options"),
+    Output("country_dropdown", "value", allow_duplicate=True),
+    Output("division_dropdown", "options"),
+    Output("division_dropdown", "value", allow_duplicate=True),
+    Output("discipline_dropdown", "options"),
+    Output("discipline_dropdown", "value", allow_duplicate=True),
+    Output("age_dropdown", "options"),
+    Output("age_dropdown", "value", allow_duplicate=True),
     Input("event_dropdown", "value"),
     prevent_initial_call=True,
 )
@@ -27,7 +43,71 @@ def on_event_update(selected_event):
         print(selected_event)
         event_data = get_event_data_from_json(selected_event)
         df = parse_event_data(event_data)
-        return PxLineChart(df=df)
+        figure = PxLineChart(df=df)
+
+        names_options = [
+            {
+                "label": html.Span(
+                    className="text-xs lg:text-sm whitespace-nowrap",
+                    children=[
+                        html.Span(
+                            className="font-bold w-4",
+                            children=f" #{rank_index+1}",
+                        ),
+                        html.Span(
+                            className="",
+                            children=f" {flag.flag(rider['country'])}",
+                        ),
+                        html.Span(
+                            className="",
+                            children=f" {rider['name']}",
+                        ),
+                        html.Span(
+                            className="text-xs italic align-right",
+                            children=f" {rider['miles'][-1]:.1f} miles",
+                        ),
+                    ],
+                ),
+                "value": rider["name"],
+            }
+            for rank_index, rider in enumerate(event_data["riders"])
+        ]
+        names_value = [rider["name"] for rider in event_data["riders"]]
+        print(f"names value for new dataset : {names_value}")
+
+        country_options = [
+            {"label": country, "value": country} for country in df["country"].unique()
+        ]
+        country_value = [rider["country"] for rider in event_data["riders"]]
+
+        division_options = [
+            {"label": division, "value": division}
+            for division in df["division"].unique()
+        ]
+        division_value = [rider["division"] for rider in event_data["riders"]]
+
+        discipline_options = [
+            {"label": discipline, "value": discipline}
+            for discipline in df["discipline"].unique()
+        ]
+        discipline_value = [rider["discipline"] for rider in event_data["riders"]]
+
+        age_options = [{"label": age, "value": age} for age in df["age"].unique()]
+        age_value = [rider["age"] for rider in event_data["riders"]]
+
+        return (
+            figure,
+            names_options,
+            names_value,
+            country_options,
+            country_value,
+            division_options,
+            division_value,
+            discipline_options,
+            discipline_value,
+            age_options,
+            age_value,
+        )
 
 
 @app.callback(
@@ -37,6 +117,7 @@ def on_event_update(selected_event):
     Output("division_dropdown", "value"),
     Output("discipline_dropdown", "value"),
     Output("age_dropdown", "value"),
+    Input("event_dropdown", "value"),
     Input("names_checklist", "value"),
     Input("country_dropdown", "value"),
     Input("division_dropdown", "value"),
@@ -48,6 +129,7 @@ def on_event_update(selected_event):
     prevent_initial_call=True,
 )
 def on_filter_update(
+    selected_event,
     selected_riders,
     selected_countries,
     selected_divisions,
@@ -56,9 +138,21 @@ def on_filter_update(
     top_10_clicks,
     all_riders_clicks,
     names_options,
-):
+) -> tuple[PxLineChart, list[str], list[str], list[str], list[str], list[str]]:
     print(">>>>>>> ON FILTER UPDATE")
     print("Triggered by", ctx.triggered_id)
+
+    print(f"Selected event : {selected_event}")
+    if ctx.triggered_id == "event_dropdown":
+        event_data = get_event_data_from_json(selected_event)
+        df = parse_event_data(event_data)
+        filtered_df = df[(df["rank"] >= 1) & (df["rank"] <= 10)]
+        top_10_riders_selected = get_riders_list_from_df(filtered_df)
+        figure = PxLineChart(df=filtered_df)
+        return figure, top_10_riders_selected, None, None, None, None
+
+    event_data = get_event_data_from_json(selected_event)
+    df = parse_event_data(event_data)
 
     filtered_df = df.copy()
 
@@ -73,13 +167,20 @@ def on_filter_update(
 
         if button_id == "top_10_riders_button":
             filtered_df = df[(df["rank"] >= 1) & (df["rank"] <= 10)]
-            top_10_riders = filtered_df["name"].unique().tolist()
+            top_10_riders_selected = get_riders_list_from_df(filtered_df)
             figure = PxLineChart(df=filtered_df)
-            return figure, top_10_riders, None, None, None, None
+            return figure, top_10_riders_selected, None, None, None, None
 
         elif button_id == "all_riders_button":
-            all_riders = filtered_df["name"].unique().tolist()
-            return PxLineChart(df=filtered_df), all_riders, None, None, None, None
+            all_riders_selected = get_riders_list_from_df(df)
+            return (
+                PxLineChart(df=filtered_df),
+                all_riders_selected,
+                None,
+                None,
+                None,
+                None,
+            )
 
     # Apply filters
     filters = [
@@ -91,13 +192,15 @@ def on_filter_update(
 
     for filter in filters:
         if filter["content"]:
-            if not isinstance(filter["content"], list):
-                content = [filter["content"]]
-            else:
-                content = filter["content"]
+            content = (
+                filter["content"]
+                if isinstance(filter["content"], list)
+                else [filter["content"]]
+            )
             filtered_df = filtered_df[filtered_df[filter["col_name"]].isin(content)]
 
     updated_checklist_values = filtered_df["name"].unique().tolist()
+    print(f"updated selected names after filters : {updated_checklist_values}")
 
     return (
         PxLineChart(df=filtered_df),
